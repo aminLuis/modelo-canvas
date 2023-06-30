a56127671ffe125f0ac52e4ac0f8c57325a9c0ce
aminLuis <aminluisamin@gmail.com>
2023-06-29 21:58:17 -0500

    Configuración routing para componentes StandAlone

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { CanvasComponent } from './pages/canvas/canvas.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'inicio', component:InicioComponent},
+  {path: 'canvas', component:CanvasComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/pages/canvas/canvas.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-canvas',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './canvas.component.html',
+  styleUrls: ['./canvas.component.scss']
+})
+export class CanvasComponent {
+
+}
