facbcd7db0ad46b2ae0bfbbad86729ae1afc3a1b
aminLuis <aminluisamin@gmail.com>
2023-07-02 22:57:13 -0500

    Consumo de servicio get_canvas_model funcionando

--- src/app/pages/canvas/canvas.component.ts
+++ src/app/pages/canvas/canvas.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { CanvasModelServiceService } from 'src/app/services/canvas-model-service.service';
+import { model_canvas } from 'src/app/interfaces/canvas-model.interface';
 
 @Component({
   selector: 'app-canvas',
@@ -10,6 +12,24 @@ import {MatIconModule} from '@angular/material/icon';
   templateUrl: './canvas.component.html',
   styleUrls: ['./canvas.component.scss']
 })
-export default class CanvasComponent {
+
+
+export default class CanvasComponent implements OnInit {
+
+  public model_canvas:model_canvas[]=[];
+
+  constructor(private model_canvas_service:CanvasModelServiceService){
+
+  }
+
+  ngOnInit(): void {
+      this.get_canvas_models();
+  }
+
+  public get_canvas_models(){
+    this.model_canvas_service.get_canvas_models().subscribe(res=>{
+      console.log(res);
+    });
+  }
 
 }
